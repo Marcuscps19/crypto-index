03a889bb7f45a3a4f826d03fb1c84d299e37ba08
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-15 19:04:36 -0300

    feat(routes/crypto.js): adiciona middleware de validacao de token nas rotas

--- back-end/routes/crypto.js
+++ back-end/routes/crypto.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { validatePayload } = require('../middlewares/crypto');
+const validateToken = require('../middlewares/authToken');
 const { getCurrencies } = require('../controllers/crypto');
 const { updateCurrency } = require('../controllers/crypto');
 
 const route = express.Router();
 
-route.get('/', getCurrencies);
-route.post('/', validatePayload, updateCurrency);
+route.get('/', validateToken, getCurrencies);
+route.post('/', validateToken, validatePayload, updateCurrency);
 
 module.exports = route;

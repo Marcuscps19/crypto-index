7fce8cc5cc8a547d524707a260a201f34cb3c29d
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-15 16:08:19 -0300

    feat(app.js): adiciona rota crypto

--- back-end/app.js
+++ back-end/app.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 
 const errors = require('./middlewares/error');
 const loginRoutes = require('./routes/login');
+const cryptoRoutes = require('./routes/crypto');
 const basePath = require('./utils/basePath');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(`${basePath}/login`, loginRoutes);
+app.use(`${basePath}/crypto/btc`, cryptoRoutes);
 app.use(errors);
 
 module.exports = app;

9e9d07b4041cf327943a6bf26cedcf850e10694d
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-15 17:31:52 -0300

    fix(routes): remove rescue das rotas

--- back-end/routes/login.js
+++ back-end/routes/login.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const rescue = require('express-rescue');
 
 const route = express.Router();
 
 const { validateLoginPayload } = require('../middlewares/login');
 const loginController = require('../controllers/login');
 
-route.post('/', validateLoginPayload, rescue(loginController.login));
+route.post('/', validateLoginPayload, loginController.login);
 
 module.exports = route;

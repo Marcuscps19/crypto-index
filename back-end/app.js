dc1659533af4b21bf0802bf8c49c7adcd5aae76c
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-15 19:03:58 -0300

    feat(app.js): adiciona rota generica

--- back-end/app.js
+++ back-end/app.js
@@ -5,12 +5,15 @@ const errors = require('./middlewares/error');
 const loginRoutes = require('./routes/login');
 const cryptoRoutes = require('./routes/crypto');
 const basePath = require('./utils/basePath');
+const genericRoute = require('./routes/genericRoute');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(`${basePath}/login`, loginRoutes);
 app.use(`${basePath}/crypto/btc`, cryptoRoutes);
+app.use(genericRoute);
+
 app.use(errors);
 
 module.exports = app;

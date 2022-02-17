8298c63f6e81e61977a80512186b3aa0efeef5b3
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-17 20:27:15 -0300

    fix(controllers/crypto.js): adiciona try catch na chamada do service de leitura de fetch

--- back-end/controllers/crypto.js
+++ back-end/controllers/crypto.js
@@ -1,4 +1,4 @@
-const { successResponses } = require('../utils/httpStatusCodes');
+const { successResponses, serverErrors } = require('../utils/httpStatusCodes');
 const { fetchCurrencies, addNewCurrencies, updateCurrencyFromFile } = require('../services/crypto');
 
 const getCurrencies = async (_req, res, next) => {
@@ -9,12 +9,17 @@ const getCurrencies = async (_req, res, next) => {
   return res.status(successResponses.ok).json(addNewCurrencies(response.data));
 };
 
-const updateCurrency = (req, res) => {
+const updateCurrency = async (req, res) => {
   const { currency, value } = req.body;
-  updateCurrencyFromFile(currency, value);
-  return res.status(successResponses.ok).json({
-    message: 'Valor alterado com sucesso!',
-  });
+  try {
+    await updateCurrencyFromFile(currency, value);
+    return res.status(successResponses.ok).json({
+      message: 'Valor alterado com sucesso!',
+    });
+  } catch (error) {
+    return res.status(serverErrors.internalSeverError)
+      .json({ message: `Erro ao salvar arquivo + ${error}` });
+  }
 };
 
 module.exports = { getCurrencies, updateCurrency };

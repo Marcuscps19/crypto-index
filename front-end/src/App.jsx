9d9b3fcbe7ebcddf6134fe2c260b05e0eb8ea6a9
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-17 12:13:22 -0300

    fix(App.jsx): corrige importaçao do provider

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,7 +5,7 @@ import RequireAuth from './contexts/RequireAuth'
 import Login from './pages/Login';
 import Home from './pages/Home';
 import UpdatePrice from './pages/UpdatePrice';
-import {HomeProvider} from './contexts/Home'
+import {CurrenciesProvider} from './contexts/Currencies'
 
 // Source Routes Authentication https://gist.github.com/mjackson/d54b40a094277b7afdd6b81f51a0393f
 function App() {
@@ -16,9 +16,9 @@ function App() {
           path="/"
           element={
           <RequireAuth redirectTo="/login">
-            <HomeProvider>
+            <CurrenciesProvider>
               <Home />
-            </HomeProvider>
+            </CurrenciesProvider>
           </RequireAuth>
         } 
         >

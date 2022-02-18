c2441d77d91d035a146945addef4a4d0b62a8146
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-18 16:33:02 -0300

    feat(pages/Login.jsx): adiciona componente de Header

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LoginContext } from '../contexts/Login';
 import axios from 'axios';
+import Header from '../components/Header';
 
 function Login() {
     const { values, setValues } = useContext(LoginContext);
@@ -32,37 +33,58 @@ function Login() {
     };
 
     return (
-        <form onSubmit={onSubmit}>
-            {/* Image Source: https://giphy.com/gifs/joinhoney-f9qwUQW56qC7BeCfCM */}
-            {/* <img src='https://media1.giphy.com/media/f9qwUQW56qC7BeCfCM/giphy.gif?cid=ecf05e477e0wmxie7c3qpjgoofgz7gby1mbyjdlqkblkrhku&rid=giphy.gif&ct=g' alt='gif de uma moeda andando' /> */}
-            <h1>Crypto App</h1>
-            <label htmlFor="email">
-                <input
-                    name="email"
-                    type="email"
-                    value={ values.email }
-                    onChange={ onChange }
-                    required
-                    placeholder="Digite seu e-mail"
-                />
-            </label>
-            <label htmlFor="password">
-                <input
-                    name="password"
-                    type="password"
-                    value={ values.password }
-                    onChange={ onChange }
-                    required
-                    placeholder="Digite sua senha"
-                />
-            </label>
-            <button
-                type="submit"
-            >
-                Entrar
-            </button>
-            <span>{values.errorMessage}</span>
-        </form>
+        <div className="container">
+           <Header />
+            <div className="container-body">
+                <div className="container-form">
+                    <h2 className="form-title">Fazer Login</h2>
+                    <form className="form-login" onSubmit={onSubmit}>
+                        <div className="form-data">
+                            <label htmlFor="email">
+                            <input
+                                autoComplete='off'
+                                name="email"
+                                type="email"
+                                value={ values.email }
+                                onChange={ onChange }
+                                required
+                                placeholder="Digite seu e-mail"
+                            />
+                            </label>
+                            <label htmlFor="password">
+                                <input
+                                    name="password"
+                                    type="password"
+                                    value={ values.password }
+                                    onChange={ onChange }
+                                    required
+                                    placeholder="Digite sua senha"
+                                />
+                            </label>
+                            <button
+                                type="submit"
+                            >
+                                Entrar
+                            </button>
+                            <span>{values.errorMessage}</span>
+                        </div>
+                    </form>
+                </div>
+                <aside className="aside-info">
+                    <h3>Verifique o valor do Bitcoin na quantidade desejada</h3>
+                    <p>Valores em:</p>
+                    <ul>
+                        <li>- Dólar americano</li>
+                        <li>- Dólar canadense</li>
+                        <li>- Euro</li>
+                        <li>- Real</li>
+                    </ul>
+                    <p>Atualize os valores da cotação do Dólar Canadense, Euro e Real.</p>
+                    {/* Image Source: https://giphy.com/gifs/joinhoney-f9qwUQW56qC7BeCfCM
+                <img src='https://media1.giphy.com/media/f9qwUQW56qC7BeCfCM/giphy.gif?cid=ecf05e477e0wmxie7c3qpjgoofgz7gby1mbyjdlqkblkrhku&rid=giphy.gif&ct=g' alt='gif de uma moeda andando' /> */}
+                </aside>
+            </div>
+        </div>
     )
 }
 

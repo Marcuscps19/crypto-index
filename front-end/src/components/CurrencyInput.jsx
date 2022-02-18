3da6eceda9283d7f645f8c59fbf12169bab3946c
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-17 21:04:01 -0300

    feat(components/CurrencyInput.jsx): adiciona formataçao no input das moedas de acordo com o codigo

--- front-end/src/components/CurrencyInput.jsx
+++ front-end/src/components/CurrencyInput.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+const formatByLocation = (local, value, code) => value
+    .toLocaleString(local, {style: 'currency', currency: code, minimumFractionDigits: 4})
+
+
+export const formatValue = (code, value) => {
+    switch (code) {
+        case 'BRL': return formatByLocation('pt-BR', value, code);
+        case 'CAD': return formatByLocation('en-CA' , value, code);
+        case 'EUR': return formatByLocation('de-DE', value, code);
+        default: return formatByLocation('en-US', value, code);
+    }
+}
+
 function CurrencyInput({code, value}) {
         return (
             <label htmlFor={code}>
                 {code}
-                <input value={value} name={code} type="text" disabled />
+                <input value={formatValue(code, value)} name={code} type="text" disabled />
             </label>
         )
 }

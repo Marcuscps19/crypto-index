629032153034550e6c98a9e8aa9b6e533e12f815
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-18 16:31:45 -0300

    feat(components/Loading.jsx): adiciona componente de Loading

---
+++ front-end/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+function Loading() {
+    return (
+        <div className="container-loader">
+            <div className="spinner-3"></div>
+            <span>Carregando...</span>
+        </div>
+    );
+}
+
+export default Loading;
+

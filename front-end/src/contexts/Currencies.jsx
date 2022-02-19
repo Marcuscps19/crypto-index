14f7479152adbb9242ddeb75552cbd367da40758
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-19 11:44:23 -0300

    refactor(contexts/Currencies.jsx): remove useEffect e funçoes de formataçao para o Home.jsx

--- front-end/src/contexts/Currencies.jsx
+++ front-end/src/contexts/Currencies.jsx
@@ -14,38 +14,6 @@ export function CurrenciesProvider({ children }) {
   const [values, setValues] = useState(initialState);
   const memoizedValues = useMemo(() => ({ values, setValues }), [values]);
 
-  useEffect(() => {
-    getCurrencies();
-  }, [values.errorMessage]);
-
-  const formatCurrencies = ((currencies) => { 
-    const values = Object.values(currencies);
-    return values.map((value) => ({...value, 'calculated_rate': value['rate_float']}))
-});
-
-  const fetchCurrencies = async () => {
-    const token = JSON.parse(localStorage.getItem('token'));
-    const headers = { headers: {
-        'Authorization': token,
-    }}
-    return await axios.get('http://localhost:3001/api/crypto/btc', headers);
-}
-  
-  const getCurrencies = async () => {
-    try {
-        const { data: { bpi } } = await fetchCurrencies()  
-        setValues({
-            ...values,
-            currencies: formatCurrencies(bpi),
-        });
-    } catch({response}) {
-        setValues({
-            ...values,
-            errorMessage: response.data.message,
-        })
-    }
-  }
-
   return (
     <CurrenciesContext.Provider value={memoizedValues}>
       { children }

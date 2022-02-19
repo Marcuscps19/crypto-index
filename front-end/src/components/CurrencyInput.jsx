f2dca713843bbfc860cd5531fdb8da5ab9860c2e
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-19 11:46:56 -0300

    fix(components/CurrencyInput): remove : depois do codigo da moeda

--- front-end/src/components/CurrencyInput.jsx
+++ front-end/src/components/CurrencyInput.jsx
@@ -4,9 +4,10 @@ const formatByLocation = (local, value, code) => value
     .toLocaleString(local, {style: 'currency', currency: code, minimumFractionDigits: 4})
 
 
+
 export const formatValue = (code, value) => {
     switch (code) {
-        case 'BRL': return formatByLocation('pt-BR', value, code);
+        case 'BRL': return (formatByLocation('pt-BR', value, code));
         case 'CAD': return formatByLocation('en-CA' , value, code);
         case 'EUR': return formatByLocation('de-DE', value, code);
         default: return formatByLocation('en-US', value, code);
@@ -16,7 +17,7 @@ export const formatValue = (code, value) => {
 function CurrencyInput({code, value}) {
         return (
             <label htmlFor={code}>
-                {code}:
+                {code}
                 <input value={formatValue(code, value)} name={code} type="text" disabled />
             </label>
         )

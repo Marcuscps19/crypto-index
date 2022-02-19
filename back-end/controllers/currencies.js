ec964b6ce16aa05e1ef70fd8b21474709cd12061
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-19 11:38:50 -0300

    feat(controllers/currencies.js): adiciona controller que verifica retorno do service de currencies para retornar erro ou o retorno

---
+++ back-end/controllers/currencies.js
@@ -0,0 +1,12 @@
+const { getActualCurrenciesFromFile } = require('../services/currencies');
+const { successResponses, serverErrors } = require('../utils/httpStatusCodes');
+
+const getActualCurrencies = async (_req, res, next) => {
+  const response = await getActualCurrenciesFromFile();
+  if (response.message) {
+    return next({ message: response.message, statusCode: serverErrors.internalServerError });
+  }
+  return res.status(successResponses.ok).json(response);
+};
+
+module.exports = { getActualCurrencies };

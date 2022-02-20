214c6735708bfeae52c51cfc4f57f3a3e93cfc27
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-20 16:32:18 -0300

    feat: configuraçoes para deploy

--- back-end/app.js
+++ back-end/app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const errors = require('./middlewares/error');
 const genericRoute = require('./routes/genericRoute');
 const routes = require('./routes/routes');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 app.use(genericRoute);
 
 app.use(errors);

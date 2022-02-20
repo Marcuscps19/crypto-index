8c02d567d77e83d995b7f0efa95bc67f809912dc
Marcus Hüsemann <mchusemann@gmail.com>
2022-02-20 10:41:10 -0300

    feat(App.jsx): importa Browser Router, remove do index.js

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import UpdatePrice from './pages/UpdatePrice';
 import {CurrenciesProvider} from './contexts/Currencies'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './reset.css'
 import './styles.css'
 
@@ -13,38 +14,40 @@ import './styles.css'
 function App() {
 
   return (
-    <Routes>
-        <Route 
-          path="/"
+    <Router>
+      <Routes>
+          <Route 
+            path="/"
+            element={
+            <RequireAuth redirectTo="/login">
+              <CurrenciesProvider>
+                  <Home />
+              </CurrenciesProvider>
+            </RequireAuth>
+          } 
+          >
+          </Route>
+          <Route
+          path="/update-price"
           element={
-          <RequireAuth redirectTo="/login">
-            <CurrenciesProvider>
-              <Home />
-            </CurrenciesProvider>
-          </RequireAuth>
-        } 
-        >
-        </Route>
-        <Route
-        path="/update-price"
-        element={
-          <RequireAuth redirectTo="/login">
-            <CurrenciesProvider>
-              <UpdatePrice />
-            </CurrenciesProvider>
-          </RequireAuth>
-        }
-        ></Route>
-        <Route
-          path="/login"
-          element= {
-          <LoginProvider>
-            <Login />
-          </LoginProvider>
+            <RequireAuth redirectTo="/login">
+              <CurrenciesProvider>
+                <UpdatePrice />
+              </CurrenciesProvider>
+            </RequireAuth>
           }
-        >
-        </Route>
-    </Routes>
+          ></Route>
+          <Route
+            path="/login"
+            element= {
+            <LoginProvider>
+              <Login />
+            </LoginProvider>
+            }
+          >
+          </Route>
+      </Routes>
+    </Router>
   );
 }
 
